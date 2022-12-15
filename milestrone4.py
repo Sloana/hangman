246a84508fba8e6075f9a4f616eae3b742e3a90a
Laureta Hajderanj <laura.hajderanj@.com>
2022-12-15 20:07:08 +0000

    Adding milwstone files to repo

---
+++ milestrone4.py
@@ -0,0 +1,30 @@
+#Task 1
+fruits=["apple","peach","banana","pear","grapes"]
+word_list=fruits
+print("My favorite fruits are:",word_list)
+confirmation = True
+guess=input("Enter a letter")
+#Task 2
+import random
+word=random.choice(word_list)
+print(word)
+len(word)
+def check_guess(guess):
+    lguess=guess.lower()
+    return lguess
+
+word_g=[]
+import _random
+class Hangman:
+    def __init__(self,word_list,num_lives=5):
+        word=random.choice(word_list)
+
+    def word_guessed(word,guess):
+        for i in len(word):
+            if word[i]==guess:
+                word_g[i]=guess
+            else:
+                word_g[i]='_'
+
+h1=Hangman(word_list)
+print(h1.word_guessed(word,guess))
